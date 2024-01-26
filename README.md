1e7a88bbf3297b73d53be2e69cdf0488454a6815
RezeScarlet <gui171204@gmail.com>
2024-01-26 12:23:17 -0300

    Test using raylib C++ Wrapper done on a older version of the project

--- README.md
+++
@@ -1,3 +0,0 @@
-# RayPong
-## Our first game using Raylib and C++
-This is a Pong clone with some extra features
--- main.cpp
+++ main.cpp
@@ -1,97 +1,127 @@
-// * Developed by
-// * Yago Miguel Nunes (Notherman)
-// :3 Clarisse Rosseti (RezeScarlet)
-
-/*
+/* 
 - Criar um menu com opções de pontuação para partidas
 - Power ups
     - Aumentar retangulo
-    - Mudar a cor do fundo do jogo (epilepsicia)
+    - Mudar a cor do fundo do jogo
     - Mudar tamanho da bola
-    - Os pontos ganham hitbox e perdem pontos se acertados
-    - Barrinha de duração
-// - Testar o wrapper de c++
-// - Player class and methods
-- Menu de configurações
-    - adaptar jogo para resoluções maiores
-    - Configuração de resolução e janela
-- FPS adaptativo
-    - configurar delta time
-- sistema de angulo para colisão
-*/
-#include "PowerUp.hpp"
-#include "Timer.hpp"
+    - O
 
+    while (WindowShouldClose() == false){
+
+        // Timer 1 seconds pontos ganham hitbox e perdem pontos se acertados
+    - Barrinha de duração
+- sistema de angulo para colisão
+- reworkar para o wrapper de C++
+*/
+#include <iostream>
+#include <raylib.h>
+#include "include/raylib-cpp.hpp"
 
 using namespace std;
 
-int main() {
+int main () {
 
+    raylib::Window window(854, 480, "RayPong");
+    SetTargetFPS(60);
     // Anti aliasing
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(854, 480, "RayPong");
-    SetTargetFPS(60);
 
-    Screen screen;
-    Timer timer;
-    Player player1(1);
-    Player player2(2);
+    raylib::Vector2 ball(100, 100);
+    int ballRadius = 15;
+    // TODO Rework ballSpeed and its incrementation
+    raylib::Vector2 ballSpeed(5, 5);
 
-    Ball ball(player1);
+  //  Rectangle player1 = {15, GetScreenHeight()/2-100, 15, 100};
+    raylib::Rectangle player1(15, GetScreenHeight()/2-50, 15, 100);
+    int player1Score = 0;
+    raylib::Rectangle player2(GetScreenWidth()-30, GetScreenHeight()/2-100, 15, 100);
+    int player2Score = 0;
 
-    PowerUp powerUp;
+    int playerSpeed = 10;
 
-    while (WindowShouldClose() == false) {
-        timer.RunTimer(GetFPS());
-        if (timer.getFrame() == 60) {
-            ball.Accelerate(0.1);
+    int frame = 0;
+    int seg = 0;
+
+    float speedEffect = 0.1;
+
+        frame++;
+        if(frame >= 60){
+            seg++;
+            frame = 0;
+            if (ballSpeed.x > 0) ballSpeed.x += speedEffect;
+            if (ballSpeed.y > 0) ballSpeed.y += speedEffect;
+            if (ballSpeed.x < 0) ballSpeed.x -= speedEffect;
+            if (ballSpeed.y < 0) ballSpeed.y -= speedEffect;
         }
 
-        ball.Collide(player1, player2);
+        // Ball moviment
+        ball.x += ballSpeed.x;
+        ball.y += ballSpeed.y;
 
-        ball.Move();
+        // Ball collision
+        // TODO turn this into a function
+        // TODO Randomize respawn angle
+        if (ball.x + ballRadius >= GetScreenWidth()) {
+            ballSpeed.x *= -1;
+            player1Score++;
+            ballSpeed.x = 5;
+            ballSpeed.y = 5;
+            ball.x = GetScreenWidth()/4;
+            ball.y = GetScreenHeight()/2;
+        }
 
-        // Player collision and Input
-        player1.Move(KeyboardKey(KEY_W), KeyboardKey(KEY_S));
-        player2.Move(KeyboardKey(KEY_UP), KeyboardKey(KEY_DOWN));
+        if (ball.x - ballRadius <= 0) {
+            ballSpeed.x *= -1;
+            player2Score++;
+            ballSpeed.x = -5;
+            ballSpeed.y = -5;
+            ball.x = GetScreenWidth()/4*3;
+            ball.y = GetScreenHeight()/2;
+        }
+
+        if (ball.y + ballRadius >= GetScreenHeight()  || ball.y - ballRadius <= 0) {
+            ballSpeed.y *= -1;
+        }
+
+        if (player1.CheckCollision(ball, ballRadius) || player2.CheckCollision(ball, ballRadius, player2)) {
+            ballSpeed.x *= -1;
+
+        }
+
+        // Player collision
+        // TODO turn this into a function
+        if (!(player1.y <= 0)) {
+            if (IsKeyDown(KEY_W)) player1.y -= playerSpeed;
+
+        }
+
+        if (!(player1.y >= GetScreenHeight() - player1.height)) {
+            if (IsKeyDown(KEY_S)) player1.y += playerSpeed;
+
+        }
+
+        if (!(player2.y <= 0)) {
+            if (IsKeyDown(KEY_UP)) player2.y -= playerSpeed;
+
+        }
+
+        if (!(player2.y >= GetScreenHeight() - player2.height)) {
+            if (IsKeyDown(KEY_DOWN)) player2.y += playerSpeed;
+        
+        }
 
         // Shape drawing
         BeginDrawing();
-
-        if (powerUp.visible) {
-            powerUp.Collide(ball);
-            DrawTriangle(powerUp.v1, powerUp.v2, powerUp.v3, WHITE);
-
-        } else if (timer.getSec() % 5 == 0){
-            powerUp.Respawn();
-        }
-
-        ClearBackground(BLACK);
-
-        DrawRectangleRec(player1.rect, WHITE);
-        DrawRectangleRec(player2.rect, WHITE);
-
-        DrawText(TextFormat("%d", player1.score), screen.w / 4, 40, 40, WHITE);
-        DrawText(TextFormat("%d", player2.score), screen.w * 3 / 4, 40, 40, WHITE);
-
-        DrawLine(screen.w / 2, 0, screen.w / 2, screen.h, WHITE);
-
-        DrawCircle(ball.GetX(), ball.GetY(), ball.radius, WHITE);
-    
-
-        // Debug stuff
-        DrawCircleV(player1.center, 3, RED);
-        DrawCircleV(player2.center, 3, RED);
-        DrawCircleV(powerUp.position, 3, RED);
-        DrawCircleV(powerUp.v1, 3, GREEN);
-        DrawCircleV(powerUp.v2, 3, BLUE);
-        DrawCircleV(powerUp.v3, 3, PURPLE);
-
-        DrawLine(0, screen.h / 2, screen.w, screen.h / 2, RED);
-
+        window.ClearBackground(BLACK);
+        player1.Draw(WHITE);
+        player2.Draw(WHITE);
+        ball.DrawCircle(ballRadius, WHITE);
+        DrawText(TextFormat("%d", player1Score), GetScreenWidth()/4, 40, 40, WHITE);
+        DrawText(TextFormat("%d", player2Score), GetScreenWidth()*3/4, 40, 40, WHITE);
+        DrawLine(GetScreenWidth()/2, 0, GetScreenWidth()/2, GetScreenHeight(), WHITE);
         EndDrawing();
     }
 
-    CloseWindow();
+    window.Close();
     return 0;
 }
